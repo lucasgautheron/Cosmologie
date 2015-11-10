2a3d2b783fb7558010dcca7f1a32ea42990aa110
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-11-10 15:33:44 +0100

    now using saxonb and XSLT 2.0

--- www/timeline/compile.php
+++ www/timeline/compile.php
@@ -7,6 +7,10 @@ function strip_decl($str)
 $files[0] = strip_decl(file_get_contents("data/events.xml"));
 $files[1] = strip_decl(file_get_contents("data/contents.xml"));
 $files[2] = strip_decl(file_get_contents("data/ressources.xml"));
+
+file_put_contents('data/cache', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}</root>");
+exec('saxonb-xslt -s:data/cache -xsl:layout.xsl -o:output/index.html -ext:on');
+/*
 $xml_doc = new DOMDocument();
 $xml_doc->loadXML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}</root>");
 
@@ -17,4 +21,4 @@ $xsl_doc->load("layout.xsl");
 // Proc
 $proc = new XSLTProcessor();
 $proc->importStylesheet($xsl_doc);
-$proc->transformToURI($xml_doc, 'output.html');
+$proc->transformToURI($xml_doc, 'output/index.html');*/
