c365af8e3dfd1da72dc4432158ad0cea31222a34
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-11-16 16:40:45 +0100

    generation time msg

--- www/timeline/compile.php
+++ www/timeline/compile.php
@@ -9,16 +9,7 @@ $files[1] = strip_decl(file_get_contents("data/contents.xml"));
 $files[2] = strip_decl(file_get_contents("data/ressources.xml"));
 
 file_put_contents('data/cache', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}</root>");
+
+$start_time = microtime(true);
 exec('saxonb-xslt -s:data/cache -xsl:layout.xsl -o:output/index.html -ext:on');
-/*
-$xml_doc = new DOMDocument();
-$xml_doc->loadXML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}</root>");
-
-// XSL
-$xsl_doc = new DOMDocument();
-$xsl_doc->load("layout.xsl");
-
-// Proc
-$proc = new XSLTProcessor();
-$proc->importStylesheet($xsl_doc);
-$proc->transformToURI($xml_doc, 'output/index.html');*/
+echo "generation done (" . round($start_tim, 4) . " s)\n";
