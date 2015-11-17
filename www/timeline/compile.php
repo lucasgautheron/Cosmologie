a0b883d76131bee1bd4b6c0d28717a683be69805
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-11-17 14:34:26 +0100

    XML references

--- www/timeline/compile.php
+++ www/timeline/compile.php
@@ -7,8 +7,9 @@ function strip_decl($str)
 $files[0] = strip_decl(file_get_contents("data/events.xml"));
 $files[1] = strip_decl(file_get_contents("data/contents.xml"));
 $files[2] = strip_decl(file_get_contents("data/ressources.xml"));
+$files[3] = strip_decl(file_get_contents("data/references.xml"));
 
-file_put_contents('data/cache', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}</root>");
+file_put_contents('data/cache', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}{$files[3]}</root>");
 
 $start_time = microtime(true);
 exec('saxonb-xslt -s:data/cache -xsl:layout.xsl -o:output/index.html -ext:on');
