6e599d14bea2f60ec6410b09c1d8303e621617e1
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2016-03-03 01:05:33 +0100

    Calculs et commentaires en vu de l'upgrade du code

--- simulations/radio/main.cpp
+++ simulations/radio/main.cpp
@@ -2,8 +2,7 @@
 #include <math.h>
 #include <omp.h>
 
-const double r0 = 1;
-const double r1 = 0.1;
+
 
 const double Vmin = -1.5;
 const double Vmax = 1.5;
@@ -16,11 +15,17 @@ const int bins = 200;
 
 #define max(a,b) ((a)>(b)? (a) : (b))
 
+// description profils de densité
+const double r0 = 1;
+const double r1 = 0.1;
+
+// masse
 double density(double r)
 {
     return exp(-r/r0);
 }
 
+// masse lumineuse HI
 double lum_density(double r)
 {
     return exp(-r/r1);
@@ -37,6 +42,8 @@ int main()
     const double dtheta = 2 * PI / double(thetasteps);
     double m = 0;
 
+    // vecteur directeur ligne de visée. A normaliser
+
     /*const double los_x = 1.0/sqrt(3);
     const double los_y = 1.0/sqrt(3);
     const double los_z = 1.0/sqrt(3);*/
@@ -51,13 +58,16 @@ int main()
         I[i] = 0;
     }
 
+    // pour chaque r entre 0 et Rmax
     for(int i = 0; i < rsteps; ++i)
     {
         double r = double(i) * dr;
         m += density(r) * 2 * PI * r * dr; 
 
+        // pour chaque theta entre 0 et 2 pi
         for(int j = 0; j < thetasteps; ++j) if(r > 0.0001)
         {
+            // calcul de la vitesse vectorielle du flot de matière en ce point (r, theta)
             double theta = double(j) * dtheta;
             double vr = sqrt(m/r);
             double vx = -vr * sin(theta), vy = vr * cos(theta);
@@ -68,6 +78,8 @@ int main()
                 printf("value out of range %f %f (%d)\n", v, m, bin);
                 continue;
             }
+            // ajoute du signal proportionnelle à la quantité de matière lumineuse dans l'élément de surface [r, r+dr[ x [theta, theta + dtheta[, 
+            // dans l'intervalle [v, v+dv[
             I[bin] += lum_density(r) * r * dr * dtheta;
         }
     }
